2800099a90bca0615a14d33461f748237688c403
xx10222 <yjw4960@gmail.com>
2021-07-29 23:44:53 +0900

    단어 1개 입력받아 그대로 출력하기

---
+++ 1021.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	char arr[51] = "";
+	scanf("%s", arr);
+	printf("%s\n", arr);
+	return 0;
+}
