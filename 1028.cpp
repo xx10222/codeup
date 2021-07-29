aa420f35557d801c2bd7a2e674a6b987b4e135c5
xx10222 <yjw4960@gmail.com>
2021-07-30 01:55:21 +0900

    정수 1개 입력받아 그대로 출력하기2

---
+++ 1028.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	unsigned int n;
+	scanf("%u", &n);
+	printf("%u", n);
+	return 0;
+}
