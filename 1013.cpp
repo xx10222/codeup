8cc3cbdf3bf66abc2a19c189d7a9e8633ecd3023
xx10222 <yjw4960@gmail.com>
2021-07-29 23:41:57 +0900

    정수 2개 입력받아 그대로 출력하기

---
+++ 1013.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int a, b;
+	scanf("%d%d", &a, &b);  //엔터/공백으로 입력 데이터가 구분되어 입력
+	printf("%d %d", a, b);
+	return 0;
+}
