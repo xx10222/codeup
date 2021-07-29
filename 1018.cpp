7b7a7b7129b388ede099c419e03d39bc1d30bc4a
xx10222 <yjw4960@gmail.com>
2021-07-29 23:43:45 +0900

    시간 입력받아 그대로 출력하기

---
+++ 1018.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int h, m;
+	scanf("%d:%d", &h, &m);
+	printf("%d:%d\n", h,m);
+	return 0;
+}
