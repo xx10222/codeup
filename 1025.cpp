b0680335ee86c2e5310987da6c297bdc0941313f
xx10222 <yjw4960@gmail.com>
2021-07-30 01:53:29 +0900

    정수 1개 입력받아 나누어 출력하기

---
+++ 1025.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int a, b, c, d, e;
+	scanf("%1d%1d%1d%1d%1d", &a, &b, &c, &d, &e);
+	printf("[%d]\n", a * 10000);
+	printf("[%d]\n", b * 1000);
+	printf("[%d]\n", c * 100);
+	printf("[%d]\n", d * 10);
+	printf("[%d]\n", e);
+	return 0;
+}
