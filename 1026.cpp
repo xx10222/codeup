21ecdb658961c55848f7a4dbcbc1ef092a14627f
xx10222 <yjw4960@gmail.com>
2021-07-30 01:54:44 +0900

    시분초 입력받아 분만 출력하기

---
+++ 1026.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int h, m, s;
+	scanf("%d:%d:%d", &h, &m, &s);
+	printf("%d\n", m);
+	return 0;
+}
