61864714515c2919e61ab44121ca2f4796e0612b
xx10222 <yjw4960@gmail.com>
2021-08-02 02:32:33 +0900

    한번에 2의 거듭제곱 배로 출력하기

---
+++ 1048.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	long long int a, b;
+	scanf("%lld %lld", &a, &b);
+	printf("%lld\n", a<<b);
+	
+	return 0;
+}
