19ca302c506a8eb005edc046c02b5939ed6595ec
xx10222 <yjw4960@gmail.com>
2021-08-02 02:30:05 +0900

    정수 1개 입력받아 부호 바꿔 출력하기

---
+++ 1040.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	long long int a;
+	scanf("%lld", &a);
+	cout << -a << endl;
+	return 0;
+}
