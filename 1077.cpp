072e2a9741863e5efe96d46842dd231d5882c196
xx10222 <yjw4960@gmail.com>
2021-08-07 19:13:59 +0900

    정수 1개 입력받아 그 수까지 출력하기

---
+++ 1077.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i <= n; i++) {
+		printf("%d\n", i);
+	}
+	
+	return 0;
+}
