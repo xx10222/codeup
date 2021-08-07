5a7afdb1c4ed25a86cdd24cfab0f12f3cdc88ee8
xx10222 <yjw4960@gmail.com>
2021-08-07 19:04:57 +0900

    정수 1개 입력받아 카운트다운 출력하기

---
+++ 1074.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	while (n != 0)
+	{
+		printf("%d\n", n);
+		n--;
+	}
+	
+	return 0;
+}
