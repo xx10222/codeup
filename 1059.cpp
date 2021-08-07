5b0ed6fa63823108a78410445420dc7975b319a4
xx10222 <yjw4960@gmail.com>
2021-08-07 19:09:42 +0900

    비트단위로 NOT 하여 출력하기

---
+++ 1059.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int a = 1;
+	scanf("%d", &a);
+	printf("%d", ~a);
+
+	
+	return 0;
+}
