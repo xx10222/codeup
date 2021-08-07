92b892ade15770c3bf8aa84a25b10f5ac428592b
xx10222 <yjw4960@gmail.com>
2021-08-07 19:07:59 +0900

    둘 다 참일 경우만 참 출력하기

---
+++ 1054.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	int num1, num2, result;
+
+
+    scanf("%d %d", &num1, &num2);
+    result = num1 && num2;
+
+    printf("%d", result);
+	
+	return 0;
+}
