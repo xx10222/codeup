4e17d077eb20351d4c53268cca54808ababcb2a8
xx10222 <yjw4960@gmail.com>
2021-08-07 19:07:38 +0900

    참 거짓 바꾸기

---
+++ 1053.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	long long int a, b;
+	scanf("%lld", &b);
+	printf("%d\n", !b);
+	
+	return 0;
+}
